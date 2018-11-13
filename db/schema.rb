2ffef5f9f9c5d2fefa49b78fabb6455c35513c73
Arthur GAUTHIER <arthurgauthier.as@gmail.com>
2018-11-13 15:33:13 +0100

    model implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_124429) do
+ActiveRecord::Schema.define(version: 2018_11_13_142521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "total_price"
+    t.string "address"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "product_lists", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_product_lists_on_order_id"
+    t.index ["product_id"], name: "index_product_lists_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "price_per_gram"
+    t.boolean "flower"
+    t.boolean "pollen"
+    t.boolean "oil"
+    t.boolean "eliquid"
+    t.boolean "availability"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +58,7 @@ ActiveRecord::Schema.define(version: 2018_11_13_124429) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
+  add_foreign_key "product_lists", "orders"
+  add_foreign_key "product_lists", "products"
 end

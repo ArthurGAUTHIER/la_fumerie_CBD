a9cee59df3805b0b3ba9aeb83376717818dda9f9
Arthur GAUTHIER <arthurgauthier.as@gmail.com>
2018-11-13 15:52:15 +0100

    validations_and_associations_implemented

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_many :orders, through: :product_list
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_per_gram, presence: true
 end

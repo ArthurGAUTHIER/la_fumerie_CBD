6ab90ce1c3ff94da0a89abc36f5d0414cd0902a4
Arthur GAUTHIER <arthurgauthier.as@gmail.com>
2018-11-14 17:37:50 +0100

    registration_setting_parmas_bug

--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,9 @@ ActiveRecord::Schema.define(version: 2018_11_14_151137) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "birth_date", default: "0", null: false
-    t.integer "age", default: 0, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

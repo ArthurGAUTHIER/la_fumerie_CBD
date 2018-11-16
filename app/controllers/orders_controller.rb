cf173d99d6c133abb52039a3a468005fd7333724
Arthur GAUTHIER <arthurgauthier.as@gmail.com>
2018-11-16 19:12:12 +0100

    routes done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,25 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show]
+
   def index
+    @orders = Order.all
   end
 
   def show
   end
 
   def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.save
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
   end
 end
